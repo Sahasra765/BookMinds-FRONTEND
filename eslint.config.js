74f262a2e6ddf3fb67fbaf858936a21db284dc10
Sahasra <23eg107e32@anurag.edu.in>
2026-03-26 01:43:24 +0530

    final fix allowed hosts

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,13 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default tseslint.config(
-  { ignores: ["dist"] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ["**/*.{ts,tsx}"],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
-      "@typescript-eslint/no-unused-vars": "off",
-    },
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true
   },
-);
+  preview: {
+    host: true,
+    allowedHosts: true
+  }
+})
